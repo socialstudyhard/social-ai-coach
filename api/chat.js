7c4ee7de4b371d56765a982917e200dd6b19df16
socialstudyhard <socialstudyhard@gmail.com>
2026-05-23 15:29:02 +0900

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,7 +1,5 @@
-export default async function handler(req, res) {
-
-  return res.status(200).json({
+export default function handler(req, res) {
+  res.status(200).json({
     message: "API 연결 성공"
   });
-
 }
