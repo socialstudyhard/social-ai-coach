2e19751dc500dcfdbd31333d8f638ce8aa29679c
socialstudyhard <socialstudyhard@gmail.com>
2026-05-23 15:27:28 +0900

    chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,18 +1,7 @@
 export default async function handler(req, res) {
-  try {
 
-    const { systemPrompt, studentInput } = req.body || {};
+  return res.status(200).json({
+    message: "API 연결 성공"
+  });
 
-    return res.status(200).json({
-      message: "서버 연결 성공",
-      hasKey: !!process.env.OPENAI_API_KEY
-    });
-
-  } catch (error) {
-
-    return res.status(500).json({
-      error: error.message
-    });
-
-  }
 }
